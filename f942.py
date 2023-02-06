df04f1da3f2f8a56118ae695d017fd5911f53ccc
XingFuTeng <2006teng@gmail.com>
2023-02-06 19:37:43 +0800

    init

---
+++ f942.py
@@ -0,0 +1,15 @@
+b=[]
+try:
+    while True:
+        a=hash(input())
+        b.append(a)
+        if len(b)>1:
+            for i in range(len(b)-1):
+                if a == b[i]:
+                    print(i)
+                    print(a)
+                    break
+except EOFError:
+    pass
+
+    
